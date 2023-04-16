5b3fca990b6a6c4905511848c5fcae99cd4b1557
v1c4r10us <v1c4r10us.29@gmail.com>
2023-04-15 20:40:59 -0500

    Updating endpoints

--- app.py
+++ app.py
@@ -5,27 +5,27 @@ import json
 app=FastAPI()
 platforms={'amazon':elt.amazon, 'disney':elt.disney, 'hulu':elt.hulu, 'netflix':elt.netflix}
 
-@app.get('/gmd/{platform}')
+@app.get('/get_max_duration/{year}/{platform}/{duration_type}')
 def get_max_duration(year:int, platform:str, duration_type:str):
     return {'movie':elt.get_max_duration(year, platforms[platform], duration_type)}
 
-@app.get('/gsc/{platform}')
+@app.get('/get_score_count/{platform}/{scored}/{year}')
 def get_score_count(platform:str, scored:float, year:int):
     return {'total_movies':elt.get_score_count(platform, scored, year)}
 
-@app.get('/gcp/{platform}')
+@app.get('/get_count_platform/{platform}')
 def get_count_platform(platform:str):
     return {'total_movies':elt.get_count_platform(platforms[platform])}
 
-@app.get('/ga/{platform}')
+@app.get('/get_actor/{platform}/{year}')
 def get_actor(platform:str, year:int):
     return {'actor': elt.get_actor(platforms[platform], year)}
 
-@app.get('/ppc')
-def prod_per_country(tipo:str, pais:str, anio:int):
-    return elt.prod_per_country(tipo, pais, anio)
+@app.get('/prod_per_country/{content_type}/{country}/{year}')
+def prod_per_country(content_type:str, country:str, year:int):
+    return elt.prod_per_country(content_type, country, year)
 
-@app.get('/gc')
+@app.get('/get_contents/{rating}')
 def get_contents(rating:str):
     return elt.get_contents(rating)
 
